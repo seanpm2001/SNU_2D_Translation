b66b4545d52b984eb6213849994728147c33e0c3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-28 17:57:01 -0700

    TOUR.js
    The Tour script for this project.

---
+++ LIBraries/Rosetta-stone-tour/TOUR.js
@@ -0,0 +1,32 @@
+// Start of script
+/* Tour.js | For: //SNU/SNU_2D_Translation/LIBraries/Rosetta-stone-tour
+*/
+// Note: I am not very experienced with JavaScript, so this script may not fully work, and may come off as bad programming. I am experimenting with mass-indents to make this easier to read.
+
+class rosettaStoneTour() {
+  
+  function tour() {
+  
+   // Currently unavailable
+   console.log("The Rosetta Stone tour is currently unavailable");
+   break;
+  
+  }
+  
+  // Divider
+  
+  int main() {
+  
+    return tour();
+    break;
+  
+  }
+
+}
+
+/* File info
+* File type: JavaScript source file (*.js)
+* File version: 1 (Wednesday, April 28th 2021 at 5:57 pm)
+* Line count (including blank lines and compiler line): 33
+*/
+// End of script
